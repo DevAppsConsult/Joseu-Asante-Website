4215975a15e4e50d3476560e05f6f8de008933ff
Isaac Bremang Darko <isaacbremang@gmail.com>
2018-10-27 09:22:24 +0000

    Updates

--- libs/validate.php
+++ libs/validate.php
@@ -13,10 +13,10 @@ if(!isset($_SESSION['user_id']))
 $myPlan = json_decode(file_get_contents(base_url().'public/user/plan/'.$_SESSION['user_id']),true); 
 if(!isset($myPlan['success']))
 {
-    header('Location:  preview-article.php?id='.$_GET['id']);
+    header('Location:  my_membership.php');
     exit;
 }
 if($myPlan['data']['status'] == 'Expired')
 {
-    header('Location:  preview-article.php?id='.$_GET['id']);
+    header('Location:  my_membership.php');
 }
